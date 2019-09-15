16a85dfc8dc1f13bbad90bbc2999a95c2a502615
BreakingSoul <54959962+BreakingSoul@users.noreply.github.com>
2019-09-15 16:18:04 +0300

    ImprovedPerfect
    Ld1-2

---
+++ PerfectNumberImproved.java
@@ -0,0 +1,19 @@
+package imperative;
+import java.util.stream.IntStream;
+
+public class PerfectNumberImproved {
+
+	public enum State {
+		DEFICIENT, PERFECT, ABUNDANT;
+	}
+
+	public static void process(Integer n) {
+		int sum=IntStream.rangeClosed(1,n/2).filter(x -> n%x==0).sum();
+		State answer = sum<=n ? (sum<n ? State.DEFICIENT : State.PERFECT) : State.ABUNDANT;
+		System.out.println("Number " + n + " is " + answer);
+	}
+
+	public static void main(String[] args) {
+		process(6);
+	}
+}
