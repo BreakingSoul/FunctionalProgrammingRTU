d46e5f290a379e33edcd12c085bbfc84f29f69d0
BreakingSoul <54959962+BreakingSoul@users.noreply.github.com>
2019-09-15 17:28:52 +0300

    LD 1-1
    LD1-1 Bugfix

--- PerfectNumber.java
+++ PerfectNumber.java
@@ -1,9 +1,10 @@
 package imperative;
+
 import java.util.HashSet;
 import java.util.Set;
 
 public class PerfectNumber {
-	
+
 	public enum STATE {
 		DEFICIENT, PERFECT, ABUNDANT;
 	}
@@ -11,43 +12,43 @@ public class PerfectNumber {
 	static void detect(int n) {
 		switch (process(n)) {
 		case DEFICIENT:
-			System.out.println("Number "+ n + " is deficient!");
+			System.out.println("Number " + n + " is deficient!");
 			break;
-			
+
 		case PERFECT:
-			System.out.println("Number "+ n + " is perfect!");
+			System.out.println("Number " + n + " is perfect!");
 			break;
-			
+
 		case ABUNDANT:
-			System.out.println("Number "+ n + " is abundant!");
+			System.out.println("Number " + n + " is abundant!");
 			break;
 		}
 	}
-	
+
 	public static STATE process(int n) {
-		Set<Integer> divisors= new HashSet<>();
-		divisors = divisors(n);
-		
-		int sum=0;
-		for (int i:divisors)
-			sum=sum+i;
-		if (sum<n*2) {
+		Set<Integer> divisors = divisors(n);
+
+		int sum = 0;
+		for (int i : divisors)
+			sum = sum + i;
+		if (sum < n * 2) {
 			return STATE.DEFICIENT;
-		} else if (sum==n*2) {
-		return STATE.PERFECT;
-		}	else 
+		} else if (sum == n * 2) {
+			return STATE.PERFECT;
+		} else
 			return STATE.ABUNDANT;
 	}
-	
+
 	public static Set<Integer> divisors(int n) {
 		Set<Integer> divisors = new HashSet<Integer>();
-		for (int divisor=1; divisor<=n; divisor++) {
-			if (n%divisor==0) 
+		for (int divisor = 1; divisor <= n; divisor++) {
+			if (n % divisor == 0) {
 				divisors.add(divisor);
+			}
 		}
 		return divisors;
 	}
-	
+
 	public static void main(String[] args) {
 		detect(1);
 	}
