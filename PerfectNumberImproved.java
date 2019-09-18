4dcfa7b7f4c56cf5402eb87cde1a4c88f3902fd0
BreakingSoul <54959962+BreakingSoul@users.noreply.github.com>
2019-09-18 14:04:52 +0300

    LD1-2  Math.sqrt( ) added + Tests passed
    Works

--- PerfectNumberImproved.java
+++ PerfectNumberImproved.java
@@ -1,4 +1,7 @@
 package imperative;
+
+import java.util.Set;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class PerfectNumberImproved {
@@ -6,14 +9,29 @@ public class PerfectNumberImproved {
 	public enum State {
 		DEFICIENT, PERFECT, ABUNDANT;
 	}
-
-	public static void process(Integer n) {
-		int sum=IntStream.rangeClosed(1,n/2).filter(x -> n%x==0).sum();
-		State answer = sum<=n ? (sum<n ? State.DEFICIENT : State.PERFECT) : State.ABUNDANT;
-		System.out.println("Number " + n + " is " + answer);
+	
+	public static Set<Integer> divisors(int n) {
+		Set<Integer> divisors = IntStream.rangeClosed(1,(int)Math.sqrt(n))
+				.filter(x -> n%x==0)
+				.boxed()
+				.collect(Collectors.toSet());
+		Set<Integer> afterSqrt = divisors.stream()
+				.map(x -> n/x)
+				.collect(Collectors.toSet());
+		divisors.addAll(afterSqrt);
+		return divisors;
 	}
 
+	public static State process(int n) {		
+		int sum = divisors(n).stream().mapToInt(i -> i.intValue()).sum()/2;
+		return sum<=n ? (sum<n ? State.DEFICIENT : State.PERFECT) : State.ABUNDANT;
+	}
+
+	static void detect(int n) {
+		System.out.println("Number " + n + " is " + process(n));
+	}
+	
 	public static void main(String[] args) {
-		process(5);
+		detect(1);
 	}
 }
