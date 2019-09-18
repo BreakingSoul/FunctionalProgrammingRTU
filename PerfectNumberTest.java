10852e4c139c8891edf9d636c77272e24f3b5bbb
BreakingSoul <54959962+BreakingSoul@users.noreply.github.com>
2019-09-18 14:16:54 +0300

    Tests for LD1-1 and 1-2
    16/16

--- PerfectNumberTest.java
+++ PerfectNumberTest.java
@@ -1,38 +1,38 @@
 package imperative;
 
-import static imperative.PerfectNumber.STATE.ABUNDANT;
-import static imperative.PerfectNumber.STATE.DEFICIENT;
-import static imperative.PerfectNumber.STATE.PERFECT;
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
+import imperative.PerfectNumber.STATE;
+import imperative.PerfectNumberImproved.State;
+
 public class PerfectNumberTest {
 
 	@Test
 	public void test6Perfect() {
-		assertEquals(PERFECT, PerfectNumber.process(6));
+		assertEquals(STATE.PERFECT, PerfectNumber.process(6));
 	}
 
 	@Test
 	public void test8Deficient() {
-		assertEquals(DEFICIENT, PerfectNumber.process(8));
+		assertEquals(STATE.DEFICIENT, PerfectNumber.process(8));
 	}
 
 	@Test
 	public void test20Abundant() {
-		assertEquals(ABUNDANT, PerfectNumber.process(20));
+		assertEquals(STATE.ABUNDANT, PerfectNumber.process(20));
 	}
 
 	@Test
 	public void test16DeficientWithIntSqrt() {
-		assertEquals(DEFICIENT, PerfectNumber.process(16));
+		assertEquals(STATE.DEFICIENT, PerfectNumber.process(16));
 	}
 
 	@Test
 	public void test1Deficient() {
-		assertEquals(DEFICIENT, PerfectNumber.process(1));
+		assertEquals(STATE.DEFICIENT, PerfectNumber.process(1));
 	}
 
 	@Test
@@ -55,4 +55,51 @@ public class PerfectNumberTest {
 		int n = 8;
 		assertArrayEquals(expected, PerfectNumber.divisors(n).toArray());
 	}
+		
+		@Test
+		public void improvedTest6Perfect() {
+			assertEquals(State.PERFECT, PerfectNumberImproved.process(6));
+		}
+
+		@Test
+		public void improvedTest8Deficient() {
+			assertEquals(State.DEFICIENT, PerfectNumberImproved.process(8));
+		}
+
+		@Test
+		public void improvedTest20Abundant() {
+			assertEquals(State.ABUNDANT, PerfectNumberImproved.process(20));
+		}
+
+		@Test
+		public void improvedTest16DeficientWithIntSqrt() {
+			assertEquals(State.DEFICIENT, PerfectNumberImproved.process(16));
+		}
+
+		@Test
+		public void improvedTest1Deficient() {
+			assertEquals(State.DEFICIENT, PerfectNumberImproved.process(1));
+		}
+
+		@Test
+		public void improvedTestDivisors1() {
+			Object[] expected = new Integer[] { 1 };
+			int n = 1;
+			assertArrayEquals(expected, PerfectNumberImproved.divisors(n).toArray());
+		}
+
+		@Test
+		public void improvedTestDivisors6() {
+			Object[] expected = new Integer[] { 1, 2, 3, 6 };
+			int n = 6;
+			assertArrayEquals(expected, PerfectNumberImproved.divisors(n).toArray());
+		}
+
+		@Test
+		public void improvedTestDivisors8() {
+			Object[] expected = new Integer[] { 1, 2, 4, 8 };
+			int n = 8;
+			assertArrayEquals(expected, PerfectNumberImproved.divisors(n).toArray());
+	}
+	
 }
